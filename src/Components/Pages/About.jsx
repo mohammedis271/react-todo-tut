e7d60d304728e356757f8a96c4fdf8e4d1f4729e
Mohammed <Mohammedis271@gmail.com>
2020-12-31 10:00:05 +0200

    Added in Todo List App to Git

---
+++ src/Components/Pages/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About Page</h1>
+      <p>This is my whole about page</p>
+    </React.Fragment>
+  );
+}
